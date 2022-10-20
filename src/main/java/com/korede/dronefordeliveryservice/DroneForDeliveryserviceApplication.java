aca93e89da4c2265cea6d72592766f1a6c619886
lim <hameed.korede.hk@gmail.com>
2022-10-20 08:43:47 +0100

    First Commit

---
+++ src/main/java/com/korede/dronefordeliveryservice/DroneForDeliveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.korede.dronefordeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneForDeliveryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneForDeliveryserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korede/dronefordeliveryservice/DroneForDeliveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korede.dronefordeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneForDeliveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
